777789d805759860424149b92d704d7dfbfd297e
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-03-24 14:12:29 +0530

    updated

--- check_root.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = @new mysqli('localhost', 'root', '');
-if ($conn->connect_error) {
-    echo "ROOT_FAILED: " . $conn->connect_error;
-} else {
-    echo "ROOT_SUCCESS";
-    $conn->close();
-}
-?>
--- check_table.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'database/db_config.php';
-$res = $conn->query("SHOW TABLES LIKE 'footer_enquiries'");
-if ($res->num_rows > 0) {
-    echo "TABLE_EXISTS";
-} else {
-    echo "TABLE_MISSING";
-}
-?>
